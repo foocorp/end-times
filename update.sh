8f1fa15e647115861b1fb938bf38e8dd6a9f67bc
Dr. Matt Lee <mattl@hey.com>
2024-10-02 20:43:20 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+git pull
 rm -rf /var/gnu-fm/nixtape/themes/2024-end-times/
 git clone https://github.com/foocorp/end-times.git /var/gnu-fm/nixtape/themes/2024-end-times/
 mkdir /var/gnu-fm/nixtape/themes/2024-end-times/templates_c/
