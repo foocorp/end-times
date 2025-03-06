4c462eceaf6d087281c773b9f44712e499782353
Dr. Matt Lee <mattl@hey.com>
2025-03-05 20:19:32 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
+echo "Updating repo"
 git pull
 rm -rf /var/gnu-fm/nixtape/themes/2024-end-times/
 git clone https://github.com/foocorp/end-times.git /var/gnu-fm/nixtape/themes/2024-end-times/
+echo "Theme updated"
 mkdir /var/gnu-fm/nixtape/themes/2024-end-times/templates_c/
 chmod 777 /var/gnu-fm/nixtape/themes/2024-end-times/templates_c/
 # apache2ctl graceful
